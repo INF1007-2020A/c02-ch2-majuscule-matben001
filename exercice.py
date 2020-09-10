6c9b15ab7e01022780f68d690468603a8db2f7b5
matben001 <benoitm007@gmail.com>
2020-09-10 21:58:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 def majuscule(mot):
     resultat = ''
     for lettre in mot:
-        lettre=chr(ord(lettre)+10
+        lettre=chr(ord(lettre)+10))
         # TODO completer la fonction ici
         
         resultat += lettre
