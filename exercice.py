500aafa8873b81b88a993667cd4a3fff53d3b349
matben001 <benoitm007@gmail.com>
2020-09-10 22:15:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     dist_entre_lettre_min_maj = ord('a') - ord('A')
     for lettre in mot:
-        if ord
+        if ord(let)
         lettre=chr(ord(lettre)- dist_entre_lettre_min_maj)
         
 
